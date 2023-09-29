2065ede9c4a9d5565b7bf6829bf015ea694558d8
thiagolopes <thiagolopes@protonmail.com>
2023-09-29 11:18:55 -0300

    Add sandcastle theme

--- packages.el
+++ packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 
 (package! gruber-darker-theme)
+(package! sandcastle-theme :recipe (:host github :repo "habamax/sandcastle-theme"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
